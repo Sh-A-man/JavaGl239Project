2bb991f33d798aa6e8d816b3855bb59e41456043
Alexei239 <8119622712@mail.ru>
2021-03-12 15:04:34 +0300

    12

--- src/main/java/problem/Parabola.java
+++ src/main/java/problem/Parabola.java
@@ -6,14 +6,25 @@ import javax.media.opengl.GL2GL3;
 import java.util.Vector;
 
 public class Parabola {
-    Vector2 A;
-    Vector2 B;
+    Vector2 F;
+    Vector2 x0;
 
-    public Parabola(Vector2 pos1, Vector2 pos2, Vector2 pos3) {
-        this.A = pos1;
-        this.B = pos2;
-        // y=ax^2+bx+c;
-        // F-это фокус параболы(он задается)
+    public Parabola(Vector2 F, Vector2 x0) {
+        this.F = F;
+        this.x0 = x0;
+        
+    }
+
+    public static void renderParabola(GL2 gl, Vector2 F, Vector2 x0, boolean field) { // F-это фокус, x0-вершина
+        if (field) {
+        Vector2 posF;
+        gl.glBegin(GL.GL_LINE_STRIP);
+        gl.glVertex2d(pos.x, pos.y);
+        int n = 3600;
+        for (int i =1; i<n; i++) {
+            double
+        }
+
         int a;
         int b;
         int c;
